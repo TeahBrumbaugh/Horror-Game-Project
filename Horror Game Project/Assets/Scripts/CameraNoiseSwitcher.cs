e642f73e5fa3fb3b1f01b25af0e3459f02656d05
KChungFBK <125692231+KChungFBK@users.noreply.github.com>
2025-05-28 17:05:18 -0700

    Super Shakey Camera
    Added script to make camera super shakey at specified time

---
+++ Horror Game Project/Assets/Scripts/CameraNoiseSwitcher.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraNoiseSwitcher : MonoBehaviour
+{
+    public CinemachineCamera virtualCamera;
+    public NoiseSettings newNoiseProfile;
+    [SerializeField] private float TotalTimer = 300f; // Time after which to switch noise profile
+
+    private CinemachineBasicMultiChannelPerlin perlin;
+
+    void Start()
+    {
+        if (virtualCamera != null && newNoiseProfile != null)
+        {
+            // Get the noise component attached to the same GameObject
+            perlin = virtualCamera.GetComponent<CinemachineBasicMultiChannelPerlin>();
+        }
+        else
+        {
+            Debug.LogWarning("No CinemachineBasicMultiChannelPerlin component found on the virtual camera.");
+        }
+    }
+
+    void Update()
+    {
+        if (perlin != null && Time.time > TotalTimer)
+        {
+            perlin.NoiseProfile = newNoiseProfile;
+        }
+    }
+}
